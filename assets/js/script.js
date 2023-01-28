78a27e1a33d1055222884989c8c2dd79c01db827
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-01-27 19:53:11 -0700

    updated html file to 5 questions. added a break between the timer and high score divs to make it easier to read. chose 15 seconds for each question and added that to the starting time. added a wrapper class to the html file to keep the text centered in the body. added classes to the css file to define the wrapper section. added a hide class to hide the home section when the start quiz button is clicked. added the javascript defining factors for the start function of the quiz. tested the files and the home screen disappears after you click the start button to prepare for the question section of the quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 console.log(window)
+
+var startBtn = document.querySelector("#start");
+
+function startQuiz() {
+  // hide start screen
+  var homeScreenEl = document.getElementById("home");
+  homeScreenEl.setAttribute("class", "hide");
+
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+// start quiz
+startBtn.onclick = startQuiz;
